bcc73a2b473b937da8db62528cb751c1f2e13c47
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-29 12:39:18 -0300

    add avatar professor

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -13,21 +13,38 @@ export function Video() {
             <h1 className="text-2xl font-bold">
               Aula 01 - Abertura do Ignite Lab
             </h1>
-            <p className="mt-12 text-gray-200">
+            <p className="mt-12 leading-relaxed text-gray-200">
               Nessa aula vamos dar inicio.......
             </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="w-16 h-16 border-2 border-blue-500 rounded-full"
+                src="https://github.com/guilhermemm-dev.png"
+                alt="github avatar"
+              />
+
+              <div className="leading-relaxed">
+                <strong className="block text-2xl font-bold">
+                  Guilherme Martins
+                </strong>
+                <strong className="block text-sm text-gray-200">
+                  Front-end Developer
+                </strong>
+              </div>
+            </div>
           </div>
           <div className="flex flex-col gap-4">
             <a
               href="#"
-              className="flex items-center justify-center gap-2 p-4 text-sm font-bold uppercase bg-green-500 rounded"
+              className="flex items-center justify-center gap-2 p-4 text-sm font-bold uppercase transition-colors bg-green-500 rounded hover:bg-green-700"
             >
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
             <a
               href="#"
-              className="flex items-center justify-center gap-2 p-4 text-sm font-bold text-blue-500 uppercase border border-blue-500 rounded"
+              className="flex items-center justify-center gap-2 p-4 text-sm font-bold text-blue-500 uppercase transition-colors border border-blue-500 rounded hover:bg-blue-500 hover:text-gray-900 "
             >
               <Lightning size={24} />
               Acesse o desafio
