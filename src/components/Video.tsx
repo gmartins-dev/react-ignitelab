cd0838dac9bfd7778a4f0362e4ce05a3cbfb1ce5
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-06-29 12:58:59 -0300

    instalada e configurada vimejs

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,5 @@
+import { DefaultUi, Player, Youtube } from '@vime/react';
+import '@vime/core/themes/default.css';
 import {
   CaretRight,
   DiscordLogo,
@@ -9,7 +11,12 @@ export function Video() {
   return (
     <div className="flex-1">
       <div className="flex justify-center bg-black">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="xMVzdUUqPqs" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
