3a37a98942a7c820db31e862b2d2d2d4930ba527
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-02 09:24:28 -0300

    Adicionado Spinner component para loading

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,13 @@
-import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-
-import '@vime/core/themes/default.css'
-import { useGetLessonBySlugQuery } from "../graphql/generated";
+import { DefaultUi, Player, Youtube } from '@vime/react';
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from 'phosphor-react';
+import { Spinner } from './Spinner';
+import '@vime/core/themes/default.css';
+import { useGetLessonBySlugQuery } from '../graphql/generated';
 
 interface VideoProps {
   lessonSlug: string;
@@ -12,15 +17,11 @@ export function Video(props: VideoProps) {
   const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug,
-    }
-  })
+    },
+  });
 
   if (!data || !data.lesson) {
-    return (
-      <div className="flex-1">
-        <p>Carregando...</p>
-      </div>
-    )
+    return <Spinner />;
   }
 
   return (
@@ -53,20 +54,30 @@ export function Video(props: VideoProps) {
                 />
 
                 <div className="leading-relaxed">
-                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
                 </div>
               </div>
             )}
           </div>
 
           <div className="flex flex-col gap-4">
-            <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+            <a
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
 
-            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+            <a
+              href=""
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
               <Lightning size={24} />
               Acesse o desafio
             </a>
@@ -74,14 +85,20 @@ export function Video(props: VideoProps) {
         </div>
 
         <div className="gap-8 mt-20 grid grid-cols-2">
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={24} />
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Material complementar</strong>
+              <strong className="text-2xl">
+                Material complementar
+              </strong>
               <p className="text-sm text-gray-200 mt-2">
-                Acesse o material complementar para acelerar o seu desenvolvimento
+                Acesse o material complementar para acelerar
+                o seu desenvolvimento
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
@@ -89,14 +106,20 @@ export function Video(props: VideoProps) {
             </div>
           </a>
 
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={24} />
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <strong className="text-2xl">
+                Wallpapers exclusivos
+              </strong>
               <p className="text-sm text-gray-200 mt-2">
-                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                Baixe wallpapers exclusivos do Ignite Lab e
+                personalize a sua máquina
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
